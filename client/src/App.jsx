53025340322e521a21ef974c05b0e29deb2f2ea7
AbhishekDevMode <itsabhishek5678@gmail.com>
2026-06-21 16:04:38 +0530

    Login page created

---
+++ client/src/App.jsx
@@ -0,0 +1,29 @@
+import { useState } from "react";
+import "./App.css";
+import Login from "./components/Login";
+import { BrowserRouter ,Routes,Route,Navigate} from "react-router-dom";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+  
+      <BrowserRouter>
+         <div className="min-h-screen bg-slate-50 font-sans">
+        <nav className="bg-white border-b border-slate-200 px-6 py-4 flex items-center justify-between shadow-sm">
+          <div className="flex items-center gap-2 text-primary font-bold text-xl tracking-tight">
+            <span className="bg-blue-600 text-white p-1 rounded-lg">🏥</span> HealthSync
+          </div>
+        </nav>
+        
+        <Routes>
+          <Route path="/" element={<Navigate to="/login" replace />} />
+          <Route path="/login" element={<Login />} />
+          {/* <Route path="/dashboard" element={<Dashboard />} /> */}
+        </Routes>
+      </div>
+      </BrowserRouter>
+  );
+}
+
+export default App;
